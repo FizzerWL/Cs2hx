615a6fc8a3d2e89b52becbec5a353dbc629a41c3
Fizzer <randyficker@gmail.com>
2019-01-24 19:28:14 -0800

    Implemented base 16 int parsing

--- trunk/Cs2hx/WriteInvocationExpression.cs
+++ trunk/Cs2hx/WriteInvocationExpression.cs
@@ -240,7 +240,11 @@ namespace Cs2hx
             //If we invoke a method with type parameters that aren't used in the argument list, the haxe function won't have a way to see what args were used. To give it a way, add those as parameters at the end
             foreach (var typePrm in Utility.PassTypeArgsToMethod(methodSymbol))
             {
-                var name = invocationExpression.Expression.DescendantNodesAndSelf().OfType<GenericNameSyntax>().ToList();
+                var name = invocationExpression.Expression.ChildNodes().OfType<GenericNameSyntax>().ToList();
+                if (name.Count == 0)
+                    name = invocationExpression.Expression.DescendantNodesAndSelf().OfType<GenericNameSyntax>().ToList(); //TODO: This will pick up false positives, we need a proper recursion function here
+                if (name.Count != 1)
+                    throw new Exception("Expected a single generic name, got " + string.Join(", ", name) + "  " + Utility.Descriptor(invocationExpression));
                 if (name.Count > 0 && name.Single().TypeArgumentList.Arguments.Count > 0)
                 {
                     var typePrmIndex = methodSymbol.TypeParameters.IndexOf(methodSymbol.TypeParameters.Single(o => o == typePrm));
--- trunk/system/Convert.hx
+++ trunk/system/Convert.hx
@@ -11,20 +11,25 @@ class Convert
 	
 	public static function ToByte_String_Int32(s:String, base:Int):Int
 	{
-		return throw new NotImplementedException();
+		return ToInt32_String_Int32(s, base);
 	}
 	public static function ToInt32_String_Int32(s:String, base:Int):Int
 	{
-		return throw new NotImplementedException();
+		if (base == 16)
+		{
+			return Std.parseInt("0x" + s);
+		}
+		else
+			return throw new NotImplementedException("ToInt32_String_Int32 base=" + base);
 	}
 	
 	public static function ToBase64String(bytes:Bytes):String
 	{
-		return throw new NotImplementedException();
+		return throw new NotImplementedException("ToBase64String");
 	}
 
 	public static function toString(i:Int, base:Int):String
 	{
-		return throw new NotImplementedException();
+		return throw new NotImplementedException("convert.base");
 	}
 }
